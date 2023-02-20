4ad20a9ba9518517793e17029ce04e12488ca2d4
Turtlecap1324 <72718544+Ashwin-Iyer1@users.noreply.github.com>
2023-02-19 18:49:27 -0600

    Added the option to check specific plates

--- plate-checker.py
+++ plate-checker.py
@@ -1,30 +1,32 @@
 import requests
 from itertools import combinations
-
+specific_plate = input("do you want to check a specific plate: ")
 url = 'https://www.myplates.com/api/licenseplates/passenger/lone-star-black/'
+while specific_plate == 'y' or specific_plate == 'Y':
+    plate = input('Enter the name of the plate: ')
 
-# plate = input('Enter the name of the plate')
+    if len(plate) > 6:
+        print("Plate is too long")
+        plate = input('Enter the name of the plate: ')
+    elif len(plate) < 1:
+        print("Plate is empty")
+        plate = input('Enter the name of the plate: ')
 
-# if len(plate) > 6:
-#     print("Plate is too long")
-#     plate = input('Enter the name of the plate')
-# elif len(plate) == 1:
-#     print("Plate is empty")
-#     plate = input('Enter the name of the plate')
+    response = requests.get(url+plate).text
 
-# response = requests.get(url+plate).text
+    if(response[11:12] =='a'):
+        print('\033[0;32m' + plate +  " Available"+'\033[0m')
+    else:
+        print('\033[91m' + plate +  " Invalid"+'\033[0m')
 
-# if(response[11:12] =='a'):
-#     print(Fore.GREEN + plate + "Available")
-# else:
-#     print(Fore.RED + plate + "Invalid")
+
 
 
 chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K','L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 i = 1
 res = []
 
-for sub in range(2, 5):
+for sub in range(0, 5):
     res.extend(combinations(chars, sub + 1))
 def convertTuple(tup):
     str = ''
@@ -33,11 +35,11 @@ def convertTuple(tup):
     return str
 def getresponse(url):
     if(url[11:12] =='a'):
-        print('\033[0;32m'+convertTuple(plates) + " Available")
+        print('\033[0;32m'+  convertTuple(plates) + " Available"+'\033[0m')
         with open("available-plates.txt", "a") as file1:
             file1.write(convertTuple(plates) + "\n")
     else:
-        print('\033[91m'+convertTuple(plates) + " Invalid")
+        print('\033[91m'+ convertTuple(plates) + " Invalid"+'\033[0m')
 for plates in res:
     if(len(plates) == 1):
         response = requests.get(url+plates[0]).text
