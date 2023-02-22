3ab1698d274d922ec638f11bb90fa756c1d8e9ae
Ashwin-Iyer1 <ashwiniyer06@gmail.com>
2023-02-22 13:08:06 -0600

    Add numbers to the checker

--- plate-checker.py
+++ plate-checker.py
@@ -22,7 +22,7 @@ while specific_plate == 'y' or specific_plate == 'Y':
 
 
 
-chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K','L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K','L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 i = 1
 res = []
 
